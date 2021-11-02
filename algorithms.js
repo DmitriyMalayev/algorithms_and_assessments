660492f3884bb5a8f26b54dc3cadc32cf9d2c8b1
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-11-02 02:15:52 -0400

    refactor

--- algorithms.js
+++ algorithms.js
@@ -395,6 +395,28 @@ function stepsMaker2(n, row = 0, stair = "") {
 
 
 
+function stepsMaker2(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1, ""); 
+  }
+  const add = stair.length <= row ? "#" : " "
+  steps(n, row, stair + add);
+}
+
+
+
+
+
+
+
+
+
+
 
 /*
 RECURSION 
