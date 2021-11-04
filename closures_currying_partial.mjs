da1a1f15dd5d72658d7f2036abb9214a8b5877d9
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-11-04 18:05:01 -0400

    update

--- closures_currying_partial.mjs
+++ closures_currying_partial.mjs
@@ -59,6 +59,37 @@ console.log(bake("cake")("chocolate")("vanilla"));
 console.log(bake("cupcake")("chocolate")("cherry"));
 console.log(bake("muffin")("carrot")("vanilla"));
 
-
 // Real World Example
 
+import fetch from "node-fetch";
+
+let jsonPlaceHolder = async (endpoint) => {
+  let url = `http://jsonplaceholder.typicode.com/${endpoint}`;
+  let resp = await fetch(url); //await let the fetch run before assigning a value to the response
+  let data = await resp.json(); //json string and convert it into an object
+
+  return (num) => {
+    return data
+      .slice(0, num)
+      .map((item) => {
+        let label = item.name || item.title;
+        return `<p>${endpoint} :: ${label}</p>`;
+      })
+      .join("\n");
+  };
+};
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+async function init() {
+  let posts = await jsonPlaceHolder("posts");
+  let users = await jsonPlaceHolder("users");
+
+  sleep(3000).then(() => {
+    console.log(posts(3));
+    console.log(users(3));
+  });
+}
+
+init();
