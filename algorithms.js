a868cfba677a008a59c3302756cc0a58c4f4f322
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-11-13 00:30:11 -0500

    merged

--- algorithms.js
+++ algorithms.js
@@ -827,3 +827,71 @@ class secondQueue {
     this.data.unshift(record);
   }
 }
+/* 
+Implement a 'peek' method in this Queue class.
+Peek should return the last element (the next
+one to be returned) from the queue *without*
+removing it. 
+*/
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+    //accessing the last item of the array
+  }
+}
+
+module.exports = Queue;
+
+/*
+Directions
+ 1) Complete the task in weave/queue.js
+ 2) Implement the 'weave' function.  Weave receives two queues as arguments and combines the contents of each into a new, third queue.
+ The third queue should contain the *alterating* content of the two queues.  The function should handle queues of different lengths without inserting 'undefined' into the new one.
+ 
+ *Do not* access the array inside of any queue, only use the 'add', 'remove', and 'peek' functions.
+    const queueOne = new Queue();
+      queueOne.add(1);
+      queueOne.add(2);
+    const queueTwo = new Queue();
+      queueTwo.add('Hi');
+      queueTwo.add('There');
+    const q = weave(queueOne, queueTwo);
+      q.remove() // 1
+      q.remove() // 'Hi'
+      q.remove() // 2
+      q.remove() // 'There'
+*/
+const Queue = require("./queue");
+
+function weave(sourceOne, sourceTwo) {
+  const newQueue = new Queue();
+  //while loop to check while it's not undefined it should continue iterating
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      //checking again because one of the sources can still have elements
+      newQueue.add(sourceOne.remove()); //remove is using the pop method to return the last element and we use that element to add to the queue.
+    }
+    if (sourceTwo.peek()) {
+      newQueue.add(sourceTwo.remove());
+    }
+  }
+  return newQueue;
+}
+
+module.exports = weave;
+
+
+
