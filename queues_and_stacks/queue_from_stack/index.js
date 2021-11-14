5448a99248d085fa8c9dc8595f4e3035ffba656a
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-11-14 17:48:57 -0500

    stack notes

--- queues_and_stacks/queue_from_stack/index.js
+++ queues_and_stacks/queue_from_stack/index.js
@@ -23,40 +23,48 @@ Remove Method
    Before returning the record we have to restore everything back to the first stack in case 
 Peek Method
   Similar to the remove method with the exception that instead of popping the second stack we will just peek at it. 
-*/ 
-
+*/
 
 const Stack = require("./stack");
 
 class Queue {
   constructor() {
-    this.first = new Stack();
+    this.first = new Stack(); //Created two stacks and assigned them to the queue
     this.second = new Stack();
   }
 
   add(record) {
+    //Taking a record and push it to the first stack. After adding all of our records and then if we call remove() on it, it will remove the first record added.
     this.first.push(record);
   }
 
   remove() {
+    // We cannot iterate through a stack we can only peek. For every record that is in the first stack we take it out and push it to the second stack.
+    // The while loop will run until it no longer returns a truthy value.
     while (this.first.peek()) {
       this.second.push(this.first.pop());
     }
+    // We are not immediately returning the record, we only create a reference to it.
+    // We want because we first need to restore the stack to it's original version, so it's ready for the next call to remove() method.
     const record = this.second.pop();
 
+    // Same loop but for the second stack.
     while (this.second.peek()) {
       this.first.push(this.second.pop());
     }
-    return record;
+    return record; //return the record
   }
 
+  
   peek() {
+    // while there are still records in the first stack, we take the record and put it into the second stack
     while (this.first.peek()) {
       this.second.push(this.first.pop());
     }
-
+    // Getting a record to the top record inside of the second stack
     const record = this.second.peek();
 
+    // while there are records in the second stack, we move it to the first stack. 
     while (this.second.peek()) {
       this.first.push(this.second.pop());
     }
