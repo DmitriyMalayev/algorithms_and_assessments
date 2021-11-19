404c3c5a72ce8ddf00694ec9aae464b82ee8ca2a
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-11-18 19:02:09 -0500

    notes

--- reading_and_writing_csv/argv.js
+++ reading_and_writing_csv/argv.js
@@ -1,3 +1,5 @@
+const yargs = require("yargs");
+
 console.log(process.argv);
 // In Node.js all command line arguments received by the shelll are given to the process in an array called  argv("argument values")
 // Node.js exposes this array for every running process in the form of process.argv
@@ -29,3 +31,54 @@ switch (myArgs[0]) {
   default:
     console.log("Sorry, that is not something I know how to do.");
 }
+
+const argv = yargs
+  .command("lyr", "Tells whether a year is leap or not", {
+    year: {
+      description: "The year to check for",
+      alias: "y",
+      type: "number",
+    },
+  })
+  .option("time", {
+    alias: "t",
+    description: "Tell the present time",
+    type: "boolean",
+  })
+  .help()
+  .alias("help", "h").argv;
+if (argv.time) {
+  console.log("The current time is: ", new Date().toLocaleTimeString());
+}
+
+if (argv._.includes("lyr")) {
+  const year = argv.year || new Date().getFullYear();
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    console.log(`${year} is a Leap Year`);
+  } else {
+    console.log(`${year} is NOT a leap year`);
+  }
+}
+console.log(argv); 
+
+
+/*
+console.log(argv); 
+  Provides the ability to view how yards handles your arguments. 
+argv.$0
+  Contains the name of the script file which is executed
+  Example:  "$0": "my app.js"
+argv._
+  An array containing each element not attached to an option or flag. 
+  These elements are referred to as commands in yargs. 
+Note
+  Individual options or flags become properties of argv
+  Such as with argv.h and argv.time
+Note
+  Non single letter flags must be passed in as --flag 
+  Such as node myapp.js --time 
+
+SUMMARY
+
+
+*/
