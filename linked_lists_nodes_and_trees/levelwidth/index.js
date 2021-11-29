237b246a1f70ffeae6ebbf725f9387c2d2571c0e
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-11-28 19:39:13 -0500

    organization

---
+++ linked_lists_nodes_and_trees/levelwidth/index.js
@@ -0,0 +1,25 @@
+/*
+Given the root node of a tree, return
+an array where each element is the width
+of the tree at each level.
+
+Given:
+    0
+  / |  \
+1   2   3
+|       |
+4       5
+
+Answer: [1, 3, 2]
+
+Notes
+  root is a single node. 
+  We are not working with a Tree.
+
+  Whenever you see the word "width" you should associate it with a Breadth First Traversal 
+*/
+function levelWidth(root) {
+
+}
+
+module.exports = levelWidth;
