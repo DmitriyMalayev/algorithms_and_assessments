9b20b58e3779c9cfb01a6354d198351b92d6446a
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-12-04 02:10:29 -0500

    unique integer

--- hackerrank.js
+++ hackerrank.js
@@ -70,3 +70,20 @@ function matchingStrings(strings, queries) {
 }
 
 matchingStrings(strings, queries);
+
+
+
+
+function lonelyinteger(a) {
+  let newObject = {};
+  for (let num of a) {
+    newObject[num] = newObject[num] + 1 || 1;
+  }
+  for (let key in newObject) {
+    if (newObject[key] === 1) {
+      return key;
+    }
+  }
+}
+
+lonelyinteger([1,2,3,4,3,2,1])  //4 is the unique integer
