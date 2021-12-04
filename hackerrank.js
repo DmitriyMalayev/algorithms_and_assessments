ed270c3b842591071a86ce931e344892a1cdd2ae
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-12-04 01:08:20 -0500

    matching strings update

--- hackerrank.js
+++ hackerrank.js
@@ -19,11 +19,10 @@ function miniMaxSum(arr) {
 
 miniMaxSum([7, 69, 2, 221, 8974]);
 
-*/
 
 console.log("07:05:45");
 
-/*
+
 Time Conversion
 Minutes and Seconds are always the same
 Converting Hours and Remove AM or PM for Military Time
@@ -32,7 +31,7 @@ Cases
   12 AM => 00
   1 AM - 12 PM => No conversion neccessary 
   1 PM - 11 PM => Original Hour + 12
-*/
+
 
 function timeConversion(s) {
   let amPM = s.charAt(8); //12:00:00AM or PM checks first letter
@@ -53,3 +52,23 @@ function timeConversion(s) {
   }
   return militaryHour + s.substring(2, 8);
 }
+
+*/
+
+let strings = ["ab", " ab", "abc"]
+let queries = ["ab", "abc", "bc"]
+
+
+function matchingStrings(strings, queries) {
+  let memory = {};
+  for (let string of strings) {
+    memory[string] = (memory[string] || 0) + 1;
+    // undefined                         0 + 1  key "ab" value 1
+    // 1   "ab"        
+
+  }
+  //returns an array with values 
+  return queries.map((query) => memory[query] || 0);
+}
+
+matchingStrings(strings, queries) 
