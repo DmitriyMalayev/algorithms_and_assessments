561b425628f22821d89f4755b49d34f75d1b5a4a
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-12-06 02:30:54 -0500

    three palidromes

--- interview.js
+++ interview.js
@@ -11,16 +11,49 @@
 //   statues.sort((a, b) => {
 //     return a - b;
 //   });
-  
+
 //   let min = statues[0];
 //   let max = statues[statues.length - 1];
 //   let count = 0;
 
 //   for (let i = min; i < max; i++) {
-//     debugger; 
+//     debugger;
 //     if (statues.indexOf(i) === -1) {
 //       count++;
 //     }
 //   }
 //   return count;
 // }
+
+// function reverseString(str) {
+//   debugger;
+//   return str.split("").reverse().join();
+// }
+// reverseString("hello");
+
+// function palindromeTest(str) {
+//   return str.split("").every((char, index) => {
+//     debugger;
+//     return char === str[str.length - index - 1];
+//   });
+// }
+// palindromeTest("hello");
+
+// function palindromeTest2(str) {
+//   new reversed = str.split("").reverse("").join()
+//   return reversed === str
+// }
+// palindromeTest2("helloTwo")
+
+// function palindromeTest3(str) {
+//   let len = str.length
+//   let start = str.substring(0, Math.floor(len / 2)).toLowerCase()
+//   let end = str.substring(len - Math.floor(len / 2)).toLowerCase()
+
+//   let flip = end.split("").reverse().join("")
+//   debugger
+//   return start === flip
+
+// }
+
+// palindromeTest3("HelloThrees")
