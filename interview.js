b96ef8c5dc89c8b959ada177461f7142db501712
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-12-06 00:52:42 -0500

    debugging

--- interview.js
+++ interview.js
@@ -1,9 +1,26 @@
-function shapeArea(n) {
-  let area = 1
-  for (var i = 1; i < n; i++){
-    debugger
-    area += 4*i
-  }
-  return area
-}
+// function shapeArea(n) {
+//   let area = 1
+//   for (var i = 1; i < n; i++){
+//     debugger
+//     area += 4*i
+//   }
+//   return area
+// }
 
+// function makeArrayConsecutive(statues) {
+//   statues.sort((a, b) => {
+//     return a - b;
+//   });
+  
+//   let min = statues[0];
+//   let max = statues[statues.length - 1];
+//   let count = 0;
+
+//   for (let i = min; i < max; i++) {
+//     debugger; 
+//     if (statues.indexOf(i) === -1) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
