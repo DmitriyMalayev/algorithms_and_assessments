38c290269e15aba537c75361bd7816bc3c46306a
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-12-08 23:27:25 -0500

    pyramid update

--- interview.js
+++ interview.js
@@ -271,8 +271,41 @@
 // pyramidBuilder1(3);
 
 function pyramidBuilder2(n, row = 0, level = "") {
-  const midpoint = Math.floor((2 * n - 1) / 2);
   if (row === n) {
     return;
   }
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramidBuilder2(n, row + 1);
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  let add;
+  if (midpoint - row <= level.length && midpoint.length >= level.length) {
+    add = "";
+  } else {
+    add = " ";
+  }
+  pyramidBuilder2(n, row, level + add);
+}
+pyramidBuilder2(3);
+
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  pyramid(n, row, level + add);
 }
