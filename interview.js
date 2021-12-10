6062fb32cd6aa96af411d1a582c96ce36387b638
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-12-10 02:09:10 -0500

    update

--- interview.js
+++ interview.js
@@ -325,3 +325,26 @@
 //   const matches = str.match(/[aeiou]/gi);
 //   return matches ? matches.length : 0;
 // }
+
+function matrix(n) {
+  const results = [];
+  for (i = 0; i < n; i++) {
+    results.push([]);
+  }
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+  }
+  for (let i = startRow; i <= endRow; i++) {
+    results[i][endColumn] = counter;
+  }
+}
