0992f3329355188b54a501a36af7369825cf48a5
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-04-27 22:48:10 -0400

    lonelyInteger

---
+++ interview_prep/hacker-rank-assessments/lonelyInteger.js
@@ -0,0 +1,18 @@
+function plusMinus(arr) {
+    let positives = 0
+    let negatives = 0 
+    let zeros = 0
+   for (let i = 0; i < arr.length; i++){
+       if (arr[i] > 0){
+           positives ++
+       } else if (arr[i] < 0){
+           negatives ++
+       } else {
+           zeros ++
+       }
+   }
+   console.log(positives/arr.length + "\n", negatives/arr.length + "\n", zeros/arr.length + "\n")
+//    console.log(negatives/arr.length)
+//    console.log(zeros/arr.length)
+   
+   }
