5fcb2cbccfd27a0d0df6a6f8d6736ad11e1dc655
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-04-27 22:48:31 -0400

    minimaxsum

---
+++ interview_prep/hacker-rank-assessments/miniMaxSum.js
@@ -0,0 +1,25 @@
+function miniMaxSum(arr) {
+  let minSlice = arr
+    .sort((a, b) => {
+      return a - b;
+    })
+    .slice(1);
+  let min = minSlice.reduce((val, newValue) => val + newValue, 0);
+  let maxSort = arr.sort((a, b) => {
+    return a - b;
+  });
+  let max = maxSort
+    .reverse()
+    .slice(1)
+    .reduce((val, newValue) => val + newValue, 0);
+
+  console.log(max, min);
+}
+
+miniMaxSum([7, 69, 2, 221, 8974]);
+
+console.log("07:05:45");
+
+
+
+
