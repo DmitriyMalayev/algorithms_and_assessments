67b3dc6206a30f4579bca606165cb282eabd1512
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-04-28 00:15:57 -0400

    updated

--- interview_prep/hacker-rank-assessments/diagonalDifference.js
+++ interview_prep/hacker-rank-assessments/diagonalDifference.js
@@ -1,12 +1,10 @@
 function diagonalDifference(arr) {
-  const length = arr.length;
-  let diag1 = 0;
-  let diag2 = 0;
+  let sum1 = 0;
+  let sum2 = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    diag1 += arr[i][i]; //primary diagonal sum
-    diag2 += arr[length - 1 - i][i]; //second dimension of array to calculate secondary diagonal
+    sum1 += arr[i][i];
+    sum2 += arr[arr.length - 1 - i][i];
   }
-  return Math.abs(diag1 - diag2);
+  return Math.abs(sum1 - sum2);
 }
-
