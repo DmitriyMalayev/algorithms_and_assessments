2e04ca2c341e131edea140b8902eb468e67d13f6
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-04-28 20:53:34 -0400

    subArrayDivision

---
+++ interview_prep/hacker-rank-assessments/subArrayDivision.js
@@ -0,0 +1,11 @@
+function birthday(s, d, m) {
+  let matches = 0
+  for (let i = 0; i < s.length; i++){
+    let subArray = s.slice(i, i + m)  //slice is non destructive
+    let sum = subArray.reduce((val, newValue) => val + newValue, 0)
+    if (sum == d) {
+      matches++
+    }
+  }
+  return matches
+}
