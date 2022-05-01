397d1c0c19c78bb5d7f2e1626575bf19fac9cbac
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-04-30 23:41:12 -0400

    validParantheses

---
+++ interview_prep/hacker-rank-assessments/validParantheses.js
@@ -0,0 +1,27 @@
+var isValid = function (s) {
+  let bracket = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  let heap = [];
+
+  for (let char of s) {
+    if (bracket[char]) {  //returns corresponding value of key 
+      heap.push(bracket[char]);
+    } else {
+      if (heap.pop() !== char) {
+        return false;
+      }
+
+      //pop removes the last element and returns it. If it's not the character that we want, return false.
+    }
+  }
+  return !heap.length; //empty string, valid.
+};
+
+// constant time ?
+// LAST IN FIRST OUT?
+
+
+isValid("(){[]}")
