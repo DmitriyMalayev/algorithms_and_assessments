7e949ae6faf66e3bedfa0b4739907f5214023dc3
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-05-08 04:06:47 -0400

    updates

---
+++ interview_prep/hacker-rank-assessments/mergeIntervals.js
@@ -0,0 +1,41 @@
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  //sorting by 0 index
+
+  const result = [intervals[0]]; //[[1,3]]
+  //first interval set as initial value of result
+
+  for (let interval of intervals) {
+    let e1 = result[result.length - 1][1]; //3  3
+    let s2 = interval[0]; //1  2
+    let e2 = interval[1]; //3  6
+
+    if (e1 >= s2) {
+      // 3 >= 1   3 >= 6
+      result[result.length - 1][1] = Math.max(e1, e2);
+      //Getting the higher value   3 = 3   3=6
+      // result[result.length - 1][1]: 3  becomes 6
+    } else {
+      result.push(interval);
+    }
+  }
+  return result;
+};
+/*
+let intervalsInput = [
+  [1, 3],
+  [2, 6],
+  [8, 10],
+  [15, 18],
+];
+
+let intervalsOutput = [
+  [1, 6],
+  [8, 10],
+  [15, 18]
+];
+
+result[result.length -1]: [1, 3]
+result[result.length -1][0]: 1
+result[result.length -1][1]: 3
+*/
--- interview_prep/hacker-rank-assessments/threeSum.js
+++ interview_prep/hacker-rank-assessments/threeSum.js
@@ -1,15 +1,15 @@
-function threeSum(nums) {
-  let solution = [];
-  let left = 0;
-  let right = nums.length - 1;
+var threeSum = function (nums) {
+  let [solution, left, right] = [[], 0, nums.length - 1];
 
   if (nums.length < 3) {
     //If there are less than three values, return empty array. Edge case.
     return solution;
   }
-  nums.sort((a, b) => a - b); //lowest to highest number
+  nums.sort((a, b) => {
+    return a - b;
+  }); //lowest to highest number
 
-  for (let [index, number] of nums.entries) {
+  for (let [index, number] of nums.entries()) {
     if (number > 0) {
       //can't add positive numbers to have a negative number
       return solution;
@@ -18,21 +18,39 @@ function threeSum(nums) {
       //continue because duplicate allowed but triplets aren't
       continue;
     }
+    //current number example -4
+
+    left = index + 1; //-1
+    right = nums.length - 1; //2
+    let temp = 0; //used to make sure the two numbers equal 0
+    
+
+
+    while (left < right) {
+      temp = number + nums[left] + nums[right];
+      if (temp === 0) {
+        solution.push([number, nums[left], nums[right]]);
+        left++; //increment and decrement because we want to account for the values used
+        right--;
+
+        //The while statements check for duplicates. If present we move.
+        while (left < right && nums[left] === nums[left - 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right + 1]) {
+          right--;
+        }
+      } else if (temp > 0) {
+        right--;
+      } else if (temp < 0) {
+        left++;
+      }
+    }
   }
-  //current number example -4
-  left = index + 1; //-1
-  right = nums.length - 1; //2
-  let temp = 0;
-}
+  return solution;
+};
 
 threeSum([-4, -1, -1, 0, 1, 2]);
-
-
-
-
-
-
-
 
 /*
 The entries() method returns a new Array Iterator object that contains the key/value pairs for each index in the array. 
